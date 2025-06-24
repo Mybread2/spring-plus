332a1bfe5c27849c96265c2c268c767a17319f31
Mybread2 <ckwnsgh4@naver.com>
2025-06-24 12:05:02 +0900

    LV 8 feat: JPQL을 QueryDSL로 변환 및 N+1 문제 해결 유지
    - TodoRepository에 QueryDSL 적용
    - findByIdWithUser를 QueryDSL 방식으로 변환
    - fetchJoin을 통한 N+1 문제 해결 유지
    - 타입 안정성과 코드 가독성 향상

--- src/main/java/org/example/expert/domain/todo/service/TodoService.java
+++ src/main/java/org/example/expert/domain/todo/service/TodoService.java
@@ -78,7 +78,7 @@ public class TodoService {
 
     @Transactional(readOnly = true)
     public TodoResponse getTodo(long todoId) {
-        Todo todo = todoRepository.findByIdWithUser(todoId)
+        Todo todo = todoRepository.findByIdWithUserUsingQueryDSL(todoId)
                 .orElseThrow(() -> new InvalidRequestException("Todo not found"));
 
         User user = todo.getUser();
